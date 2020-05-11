a250073bf5832ff127df6bf356c4437e16b202e8
SagarParekh <SagarParekh@users.noreply.github.com>
2020-05-11 16:56:32 -0700

    added dataset files

--- heatmap.js
+++ heatmap.js
@@ -1,11 +1,11 @@
 async function createHeatMap(businessId){
     // access data
-    var url = "https://raw.githubusercontent.com/chetan015/yelp-data-viz/master/tempeCheckinNew.json";
+    var url = "https://raw.githubusercontent.com/SagarParekh/Yelp_Data_Analysis/master/tempeCheckinNew.json";
     let dataset = await d3.json(url);
     var checkIns = dataset.filter( (data) => data.business_id === businessId).map(data => data.checkin)[0];
     var checkInList = []
     Object.keys(checkIns).forEach(i =>Object.values(checkIns[i]).forEach(j=>checkInList.push(j)));
-    
+
     // set margins, width
     var margin = { top: 90, right: 50, bottom: 60, left: 70};
     offsetWidth = parseInt(d3.select(".Rectangle_9").style("width").slice(0,-2));
@@ -19,13 +19,13 @@ async function createHeatMap(businessId){
     legendBoxWidth = boxSize;
     dayLabelList = ["M", "T", "W", "TH", "F", "SA", "SU"];
     timeLabelList = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"];
-    
+
     var tooltip = d3.select("body")
 	.append("div")
 	.style("position", "absolute")
 	.style("z-index", "10")
 	.style("visibility", "hidden");
-    
+
     var svg = d3.select(".Rectangle_9").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -43,7 +43,7 @@ async function createHeatMap(businessId){
           .style("font-weight","bold")
         .style("font-family","Open Sans")
           .attr("transform", "translate(-10," + boxSize / 1.5 + ")");
-          
+
     svg.selectAll(".timelabel")
           .data(timeLabelList)
           .enter().append("text")
@@ -56,12 +56,12 @@ async function createHeatMap(businessId){
     .style("font-family","Open Sans")
           .attr("fill","#081D58");
     var thresholds = [2,5,10,20,35,50,75,100];
-                      
+
     var colorScale = d3.scaleThreshold()
         .domain(thresholds)
         .range(d3.schemeYlGnBu[9]);
     var boxes = svg.selectAll(".box")
-        .data(checkInList);  
+        .data(checkInList);
         boxes.enter().append("rect")
               .attr("x", function(d,i) { return i%24 * boxSize; })
               .attr("y", function(d,i) { return Math.floor(i/24) * boxSize; })
@@ -79,8 +79,8 @@ async function createHeatMap(businessId){
               .transition().duration(function(d,i){return 100+(i%24)*50+Math.floor(i/24)*50;})
               .style("fill", function(d) { return colorScale(d); })
               ;
-                        
-    boxes.exit().remove();    
+
+    boxes.exit().remove();
     var legend = svg.selectAll(".legend").attr("class","legendMark")
     // legend.append("g");
     // var legendBox = legend.selectAll("g");
@@ -95,7 +95,7 @@ async function createHeatMap(businessId){
         .style("fill",function(d){return colorScale(d);});
 
     // legend.data(["<2"].concat(thresholds.slice(1)).concat(">=100")).enter().append('text')
-    
+
     legend.data(["<2"].concat(thresholds.slice(1)).concat(">=100")).enter().append('text')
     .attr("x",function(d,i){val = width/4.1+legendBoxWidth*(i+1);if(i==7)val-=5; else if(i==8) val-=20;return val})
     .attr("y",height-40)
@@ -107,5 +107,5 @@ async function createHeatMap(businessId){
 
     // Tooltip
 
-   
+
 }
